5c73c55b5421539ad504014c7fb4580c46b39720
Jovan Jocic <jovanjocic1993@gmail.com>
2019-05-15 23:26:07 +0200

    Added react build.... needs to be installed/finished/whatevered

---
+++ precache-manifest.697a088529d7c4c8b35f2abeb7ef0241.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c7dfa1af6563f9f9091fe4a71aa107",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94d9f554d0c2594f6eea",
+    "url": "/static/js/2.96f617a9.chunk.js"
+  },
+  {
+    "revision": "2b6105118727b15566b6",
+    "url": "/static/js/main.778dd716.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
